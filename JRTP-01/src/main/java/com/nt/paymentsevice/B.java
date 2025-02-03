96baf9ac5718db1c49b76941247599e943ae0e42
Sainath-21 <sainath2580rb@gmail.com>
2025-02-03 12:29:52 +0530

    dev sub() method is added Payment Module

--- JRTP-01/src/main/java/com/nt/paymentsevice/B.java
+++ JRTP-01/src/main/java/com/nt/paymentsevice/B.java
@@ -4,10 +4,14 @@ public class B {
 	
 	public void add()
 	{
-		
+		System.out.println("TL add() method is added Payment Module");
+
 	}
 	
-
+	public void sub()
+	{
+		System.out.println("dev sub() method is added Payment Module");
+	}
 	public static void main(String[] args) {
 
 	}
