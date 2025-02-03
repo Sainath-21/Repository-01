803fb63c8a0bef8e819cd8dd37bdc16b625c6f5d
Sainath-21 <sainath2580rb@gmail.com>
2025-02-03 12:15:27 +0530

    Dev is m1() method added

---
+++ JRTP-01/src/main/java/com/nt/commons/A.java
@@ -0,0 +1,18 @@
+package com.nt.commons;
+
+public class A {
+
+	public void m1()
+	{
+		System.out.println("m1 method from class A");
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+
+	}
+
+}
